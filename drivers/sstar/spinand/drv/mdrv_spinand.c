OpenIPC/linux

spinand: check flash type on probe

drivers/sstar/spinand/drv/mdrv_spinand.c
@@ -982,7 +982,7 @@ static int mstar_spinand_probe(struct platform_device *pdev)
 #if defined(CONFIG_MTD_CMDLINE_PARTS) || defined(CONFIG_MTD_CMDLINE_PARTS_MODULE)
         static const char *part_probes[] = { "cmdlinepart", NULL, };
 #endif
-#if defined(CONFIG_ARCH_INFINITY2)
+#if defined(CONFIG_ARCH_INFINITY2) || defined(CONFIG_ARCH_INFINITY6E)
     if(Chip_Get_Storage_Type()!= MS_STORAGE_SPINAND_ECC)
         return 0;
 #endif

wberube/openipc-linux

drivers/sstar/spinand/drv/mdrv_spinand.c
@@ -968,7 +968,7 @@ static int mstar_spinand_probe(struct platform_device *pdev)
         NULL,
     };
 #endif
-#if defined(CONFIG_ARCH_INFINITY2)
+#if defined(CONFIG_ARCH_INFINITY2) || defined(CONFIG_ARCH_INFINITY6C)
     if (Chip_Get_Storage_Type() != MS_STORAGE_SPINAND_ECC)
         return 0;
 #endif